42ca2ceb68e60d7d0375ec9aea585e36e639dcc9
DanielSanchez94 <daniel05071994@Gmail.com>
2022-07-22 09:35:04 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "List of Pokemons" ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/List of Pokemones/i);
   expect(linkElement).toBeInTheDocument();
 });
